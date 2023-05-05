06333edba3f2de7c45d4b53de67a9bd217ace42f
Edgar Diaz <ed35376@gmail.com>
2023-05-05 17:40:39 -0400

    traffic ligth done

--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -1,29 +1,38 @@
-
-import React, {useState} from 'react';
-
+import React, { useState } from "react";
 
 const Semaforo = () => {
-  const [selectColor, setSelectColor] = useState("red")
-  return(
-  <>
-  
-      <div className="Palito">
-  hola
-      </div>
+  const [selectColor, setSelectColor] = useState("red");
+  return (
+    <>
+      <div className="thin bg-dark ">1</div>
 
       <div className="trafficLight d-inline-block bg-dark p-2 rounded">
-          <div className={`light red rounded-circle bg-danger m-1 ${selectColor === "red" ? "glow" : ""}`}  onClick={() => setSelectColor('red')}  >
-  a
-          </div>
-          <div className={`light yellow rounded-circle bg-warning m-1 ${selectColor === "yellow" ? "glow" : ""}`}  onClick={() => setSelectColor('yellow')}>
-  b
-          </div>
-          <div className={`light green rounded-circle bg-success m-1 ${selectColor === "green" ? "glow" : ""}`} onClick={() => setSelectColor('green')}>
-  c
-          </div>
-      </div>    
-  
-  </>)
-  }
-  export default Semaforo
-
+        <div
+          className={`light red rounded-circle bg-danger m-1 ${
+            selectColor === "red" ? "glow" : ""
+          }`}
+          onClick={() => setSelectColor("red")}
+        >
+          
+        </div>
+        <div
+          className={`light yellow rounded-circle bg-warning m-1 ${
+            selectColor === "yellow" ? "glow" : ""
+          }`}
+          onClick={() => setSelectColor("yellow")}
+        >
+          
+        </div>
+        <div
+          className={`light green rounded-circle bg-success m-1 ${
+            selectColor === "green" ? "glow" : ""
+          }`}
+          onClick={() => setSelectColor("green")}
+        >
+          
+        </div>
+      </div>
+    </>
+  );
+};
+export default Semaforo;
